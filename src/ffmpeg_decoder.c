cb1060c75850ce69cca61ab012e6a0603512edd6
yangs1202 <me@yangs.kr>
2026-07-23 23:27:03 +0900

    gateway 영상 decode와 scale 병렬성을 높임
    Mac gateway의 단일 FFmpeg worker 병목을 제거하기 위해 decode와 filter thread 수를 4로 설정했다.

--- src/ffmpeg_decoder.c
+++ src/ffmpeg_decoder.c
@@ -89,8 +89,8 @@ bool ffmpeg_decoder_start(FfmpegDecoder* decoder, uint16_t width, uint16_t heigh
 		(void)close(input[1]);
 		(void)close(output[0]);
 		(void)close(output[1]);
-		execlp("ffmpeg", "ffmpeg", "-loglevel", "error", "-threads", "1", "-fflags",
-		      "nobuffer", "-flags", "low_delay", "-f", "h264", "-i", "pipe:0", "-an", "-pix_fmt",
+		execlp("ffmpeg", "ffmpeg", "-loglevel", "error", "-threads", "4", "-filter_threads", "4",
+		      "-fflags", "nobuffer", "-flags", "low_delay", "-f", "h264", "-i", "pipe:0", "-an", "-pix_fmt",
 		      "bgra", "-vf", scale, "-f", "rawvideo", "pipe:1", (char*)NULL);
 		_exit(127);
 	}
