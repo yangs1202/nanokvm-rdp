f2ff619e72281d3ec6e64242f66da731380ea94d
yangs <yangs@yangs-macmini-sg.local>
2026-09-26 22:26:21 +0900

    정적 RDPGFX 압축 라이브러리를 링크
    이미지 빌드가 RDPGFX 압축 심볼을 찾지 못해 FreeRDP rdtk 정적 라이브러리를 cgo에 추가한다.

--- go/internal/rdp/session_cgo.go
+++ go/internal/rdp/session_cgo.go
@@ -1,7 +1,7 @@
 package rdp
 
 /*
-#cgo pkg-config: freerdp3 freerdp-server3 winpr3
+#cgo pkg-config: freerdp3 freerdp-server3 winpr3 rdtk0
 #cgo LDFLAGS: -ldl -lpthread /usr/lib/x86_64-linux-gnu/libssl.a /usr/lib/x86_64-linux-gnu/libcrypto.a
 #cgo CFLAGS: -Wall -Wextra -Wno-deprecated-declarations
 #include "session_api.h"
